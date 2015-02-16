98b0aca971ce23d6785791d211ce91b796fa1081
akl <alexander.kovalev@opencascade.com>
2015-02-16 13:30:32 +0400

    Copyrights update 2015.

--- src/GUI/HEXABLOCKPlugin_msg_ja.ts
+++ src/GUI/HEXABLOCKPlugin_msg_ja.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 <!DOCTYPE TS>
 <!--
-  Copyright (C) 2009-2014  CEA/DEN, EDF R&D
+  Copyright (C) 2009-2015  CEA/DEN, EDF R&D
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
